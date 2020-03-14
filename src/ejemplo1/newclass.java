64b43840a44a7c61aaf2463f086aa36173476085
50683 <50683@192.168.1.4>
2020-03-13 18:22:58 -0600

    Primer comit

---
+++ src/ejemplo1/newclass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo1;
+
+/**
+ *
+ * @author 50683
+ */
+public class newclass {
+    // Programo
+    
+}
